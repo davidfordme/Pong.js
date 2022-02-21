b7d9cc129a01603e66ebf5a0b4947b65756bf97f
David Ford <d.ford@digital-velocity.co.uk>
2022-02-21 22:40:50 +0000

    Moving 1 character at a time 😄 It's a start

--- pong.js
+++ pong.js
@@ -1,4 +1,7 @@
-//Colours
+/****** STYLES *******/
+/* 
+    Colours
+*/
 const white = "#FFFFFF";
 const lightBlue = '#0C2A55';
 const darkBlue = '#111926';
@@ -31,15 +34,78 @@ root.appendChild(centerLine);
 /*
     Players
 */
-
 const player = document.createElement("div");
 player.style = "position: absolute; top: 50%; transform: translateY(-50%); height: 4rem; width: 0.5rem; background-color: " + white + ";";
 
 const player1 = player.cloneNode(true);
 player1.style['left'] = "2.5rem";
+player1.id = 'player1';
 
 const player2 = player.cloneNode(true);
 player2.style['right'] = "2.5rem";
+player2.id = 'player2';
 
 root.appendChild(player1);
 root.appendChild(player2);
+
+
+/****** LOGIC - VARIABLES *******/
+const playerPace = 3;
+const playerUpper = 15;
+const playerLower = 85;
+
+/****** LOGIC - FUNCTIONS *******/
+function KeyW() {
+    movePlayer(player1, true);
+}
+
+function KeyS() {
+    movePlayer(player1, false);
+}
+
+function ArrowUp() {
+    movePlayer(player2, true);
+}
+
+function ArrowDown() {
+    movePlayer(player2, false);
+}
+
+function Enter() {
+    console.log("START GAME?");
+}
+
+const setupInteractions = () => {
+
+    const acceptedKeys = ["KeyW", "KeyS", "ArrowUp", "ArrowDown", "Enter"];
+
+    const keyUpdate = (event) => {
+        if(acceptedKeys.includes(event.code)) {
+            event.preventDefault();
+            
+            const funct = window[event.code];
+            if(typeof funct === 'function') funct();
+        }
+    }
+
+    document.addEventListener("keydown", event => {
+        keyUpdate(event);
+    });
+}
+
+const movePlayer = (selectedPlayer, positivity) => {
+    const top = selectedPlayer.style.getPropertyValue('top');
+    let currentPosition = parseFloat(top.replace('%', ''));
+    
+    if(positivity) currentPosition = currentPosition - playerPace;
+    else currentPosition = currentPosition + playerPace;
+
+    if(currentPosition <= playerUpper) currentPosition = playerUpper;
+    if(currentPosition >= playerLower) currentPosition = playerLower;
+
+    selectedPlayer.style["top"] = currentPosition + "%";
+}
+
+window.onload = (event) => {
+    setupInteractions();
+}

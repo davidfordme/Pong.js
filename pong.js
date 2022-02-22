b3b72db78c35127ec63d8946f277061bf0f9f5fd
David Ford <d.ford@digital-velocity.co.uk>
2022-02-22 23:55:23 +0000

    Detect hit and return the puck 😄

--- pong.js
+++ pong.js
@@ -252,7 +252,24 @@ const movePuck = () => {
 }
 
 const detectHit = () => {
-    
+    if(elementsOverlap(player1, puck)) returnPuck();
+    if(elementsOverlap(player2, puck)) returnPuck();
+}
+
+const elementsOverlap = (el1, el2) => {
+    const domRect1 = el1.getBoundingClientRect();
+    const domRect2 = el2.getBoundingClientRect();
+
+    return !(
+        domRect1.top > domRect2.bottom ||
+        domRect1.right < domRect2.left ||
+        domRect1.bottom < domRect2.top ||
+        domRect1.left > domRect2.right
+    );
+}
+
+const returnPuck = () => {
+    puckHorizontalSpeed = puckHorizontalSpeed * -1;
 }
 
 const resetPuck = () => {
